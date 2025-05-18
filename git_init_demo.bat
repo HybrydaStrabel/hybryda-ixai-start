56b6b30e75eeb57af7e8783cd435b82b1255aead
HybrydaStrabel <A.Strabel@t-online.de>
2025-05-18 15:09:43 +0200

    Wersje PL, DE, EN ľ struktura startowa

---
+++ git_init_demo.bat
@@ -0,0 +1,6 @@
+git init
+git remote add origin https://github.com/HybrydaStrabel/hybryda-ixai-start.git
+git add .
+git commit -m "Wersje PL, DE, EN – struktura startowa"
+git branch -M main
+git push -u origin main
